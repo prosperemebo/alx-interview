9846dcc27aec901851943288a1de64ad12b1bf92
prosperemebo <emeboprosper@gmail.com>
2024-04-18 18:05:40 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,10 +5,10 @@
 def canUnlockAll(boxes):
     """Implementation of canUnlockAll function"""
     opened = [0]
-    
+
     for index in opened:
         for key in boxes[index]:
             if key not in opened and key < len(boxes):
                 opened.append(key)
-                
+
     return len(boxes) == len(opened)
