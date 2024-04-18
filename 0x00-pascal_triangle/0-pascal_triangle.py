39b7b962f449ccd3ffcddc31a47e5bdf52673f10
prosperemebo <emeboprosper@gmail.com>
2024-04-18 18:34:16 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,23 +8,20 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    result = []
+    result = [[1]]
+    count = 0
 
-    while len(result) < n:
-        if len(result) == 0:
-            result.append([1])
-            continue
-
-        last_row = result[len(result) - 1]
+    while count < n:
+        temp = [0] + result[count] + [0]
         row = []
 
-        for i, item in enumerate(last_row):
-            next_item = 1 if i == 0 else last_row[i - 1] + item
-            row.append(next_item)
+        for index, value in enumerate(temp):
+            nextValue = temp[index + 1] if index + 1 < len(temp) else None
 
-            if len(row) == len(last_row):
-                row.append(row[0])
+            if nextValue is not None:
+                row.append(value + nextValue)
 
         result.append(row)
+        count += 1
 
     return result
