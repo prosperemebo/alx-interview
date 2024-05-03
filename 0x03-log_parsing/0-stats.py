c77db908b2ab173815976f0a71b03769ca95dcd9
prosperemebo <emeboprosper@gmail.com>
2024-05-03 07:42:08 +0100

    New Push

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This script reads stdin line by line and computes metrics. """
+""" a script that reads stdin line by line and computes metrics """
 
 from sys import stdin
 
