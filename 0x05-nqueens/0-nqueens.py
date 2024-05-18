521dd642f3bc58c53fc1b9f1ec9b8504e8432732
prosperemebo <emeboprosper@gmail.com>
2024-05-18 22:58:05 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-""" Script for the N-Queens Challenge """
+"""
+The N queens puzzle is the challenge of placing N non-attacking queens
+on an NxN chessboard. Write a program that solves the N queens problem.
+"""
 
 import sys
 
 
 def queen_down(N, i, queens, final_combo, col, pos, neg):
     """recursive function to put down non attacking queens"""
-
     if len(queens) == N:
         final_combo.append(queens)
         return final_combo
-
     for j in range(N):
         if not (j in col or i + j in pos or i - j in neg):
-            queen_down(
-                N,
-                i + 1,
-                queens + [[i, j]],
-                final_combo,
-                col + [j],
-                pos + [i + j],
-                neg + [i - j],
-            )
+            queen_down(N, i + 1, queens + [[i, j]], final_combo,
+                       col + [j], pos + [i + j], neg + [i - j])
     return final_combo
 
 
@@ -38,11 +32,8 @@ except ValueError:
 if N < 4:
     print("N must be at least 4")
     sys.exit(1)
-
 queens = []
 col = pos = neg = []
-
 queen_down(N, 0, [], queens, col, pos, neg)
-
 for queen in queens:
     print(queen)
