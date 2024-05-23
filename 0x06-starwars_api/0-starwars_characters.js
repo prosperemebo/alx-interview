5454739650a887b785044179ce915824d1403b29
prosperemebo <emeboprosper@gmail.com>
2024-05-23 23:18:33 +0100

    Task 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+async function getCharacter(character) {
+  return new Promise((resolve, reject) => {
+    request(character, function (error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+
+async function fetchCharacters(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+
+    for (const character of characters) {
+      console.log(await getCharacter(character));
+    }
+  }
+}
+
+request(url, fetchCharacters);
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,3 @@
+# 0x06. Star Wars API
+
+0x06-starwars_api
