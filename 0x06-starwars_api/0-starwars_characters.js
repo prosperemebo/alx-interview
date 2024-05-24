8931d9ea45fe4fba97294f400358a72389fe3d96
prosperemebo <emeboprosper@gmail.com>
2024-05-24 10:10:15 +0100

    semistandard fix format errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-async function getCharacter(character) {
+async function getCharacter (character) {
   return new Promise((resolve, reject) => {
     request(character, function (error, response, body) {
       if (error) {
@@ -15,7 +15,7 @@ async function getCharacter(character) {
   });
 }
 
-async function fetchCharacters(error, response, body) {
+async function fetchCharacters (error, response, body) {
   if (!error && response.statusCode === 200) {
     const data = JSON.parse(body);
     const characters = data.characters;
